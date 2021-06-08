346b36adb3356ebbd7e39ef3b38e752115e0cbc4
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-08 10:54:41 -0300

    Add PopupMenuButton component

---
+++ src/component/PopupMenuButton/style.tsx
@@ -0,0 +1,21 @@
+import { RectButton } from "react-native-gesture-handler"
+import styled from "styled-components/native"
+
+import { styledProps } from "../../service/theme"
+
+
+export const PopupMenuButtonBase = styled(RectButton)`
+    align-items: flex-start;
+    justify-content: center;
+    padding-horizontal: 16px;
+    height: 48px;
+    background-color: ${(props: styledProps) => props.theme.color.popupMenuButton_background};
+`
+
+
+export const PopupMenuButtonText = styled.Text`
+    width: 100%;
+    font-size: 15px;
+    opacity: ${(props: styledProps) => props.theme.opacity.highEmphasis};
+    color: ${(props: styledProps) => props.theme.color.popupMenuButton_color};
+`
