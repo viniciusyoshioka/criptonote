9c2e40a7755cec397329ce0dea736f7697032289
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-08 10:17:47 -0300

    Enable theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,46 @@
-import React, { useEffect } from "react"
+import React, { useCallback, useEffect, useState } from "react"
+import { useColorScheme } from "react-native"
 import KeepAwake from "react-native-keep-awake"
+import { ThemeProvider } from "styled-components"
 
 import { Router } from "./router"
+import { readTheme, writeTheme } from "./service/storage"
+import { DarkTheme, join, LightTheme, SwitchThemeContextProvider, ThemeContextProvider, themeType } from "./service/theme"
 
 
 export function App() {
 
+
+    const deviceTheme = useColorScheme()
+    const [appTheme, setAppTheme] = useState<themeType>("auto")
+    const [theme, setTheme] = useState<themeType | undefined>()
+
+
+    const getTheme = useCallback(async () => {
+        const readAppTheme = await readTheme()
+        if (readAppTheme === "auto") {
+            if (deviceTheme) {
+                setAppTheme(readAppTheme)
+                setTheme(deviceTheme)
+                return
+            }
+            setAppTheme(readAppTheme)
+            setTheme("light")
+            return
+        }
+        setAppTheme(readAppTheme)
+        setTheme(readAppTheme)
+    }, [deviceTheme])
+
+    const switchTheme = useCallback(async (newTheme: themeType) => {
+        await writeTheme(newTheme)
+        await getTheme()
+    }, [])
+
+
+    useEffect(() => {
+        getTheme()
+    }, [deviceTheme])
 
     useEffect(() => {
         if (__DEV__) {
@@ -16,7 +51,18 @@ export function App() {
     }, [])
 
 
+    if (theme === undefined) {
+        return null
+    }
+
+
     return (
-        <Router />
+        <ThemeContextProvider value={(theme === "light") ? join(LightTheme, appTheme) : join(DarkTheme, appTheme)}>
+            <SwitchThemeContextProvider value={switchTheme}>
+                <ThemeProvider theme={(theme === "light") ? join(LightTheme, appTheme) : join(DarkTheme, appTheme)}>
+                    <Router />
+                </ThemeProvider>
+            </SwitchThemeContextProvider>
+        </ThemeContextProvider>
     )
 }
