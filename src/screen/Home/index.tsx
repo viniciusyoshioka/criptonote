d8fb4ad83b43c43d98c8798c6ff8dcbc65f627d8
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-08 18:23:28 -0300

    Add debug funtions in Home screen

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,14 +1,92 @@
-import React from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { View } from "react-native"
 
 import { SafeScreen } from "../../component/Screen"
-import { appIconOutline } from "../../service/constant"
+import { appIconOutline, appInDevelopment } from "../../service/constant"
 import { DebugHome } from "./DebugHome"
 import { HomeHeader } from "./Header"
 import { EmptyListImage, EmptyListText, EmptyListView } from "../../component/EmptyList"
+import { debugHome, Note } from "../../service/object-type"
+import { readDebugHome, readNote, readNoteId, writeDebugHome, writeNote, writeNoteId } from "../../service/storage"
+import { createAllFolder } from "../../service/folder-handler"
 
 
 export function Home() {
+
+
+    const [debugHome, setDebugHome] = useState<debugHome>("show")
+    const [note, setNote] = useState<Array<Document>>([])
+
+
+    const debugGetDebugHome = useCallback(async () => {
+        if (appInDevelopment) {
+            const getDebugHome = await readDebugHome()
+            setDebugHome(getDebugHome)
+        } else {
+            setDebugHome("hide")
+        }
+    }, [])
+
+    const debugSwitchDebugHome = useCallback(async () => {
+        switch (debugHome) {
+            case "show":
+                setDebugHome("hide")
+                await writeDebugHome("hide") 
+                break
+            case "hide":
+                setDebugHome("show")
+                await writeDebugHome("show") 
+                break
+            default:
+                throw new Error("Unknown debugHome value")
+        }
+    }, [debugHome])
+
+    const debugReadNote = useCallback(async () => {
+        const noteList = await readNote()
+        const noteId = await readNoteId()
+
+        setNote(noteList)
+
+        console.log(`note - READ - ${JSON.stringify(noteList)}`)
+        console.log(`noteId - READ - ${JSON.stringify(noteId)}`)
+    }, [])
+
+    const debugWriteNote = useCallback(async () => {
+        const tempNoteList: Array<Note> = [
+            {id: 0, title: "Titulo 1", text: "Texto 1", lastModificationDate: "18:04 08/06/2021"},
+            {id: 1, title: "Titulo 2", text: "Texto 2", lastModificationDate: "18:04 08/06/2021"},
+            {id: 2, title: "Titulo 3", text: "Texto 3", lastModificationDate: "18:04 08/06/2021"},
+            {id: 3, title: "Titulo 4", text: "Texto 4", lastModificationDate: "18:04 08/06/2021"},
+            {id: 4, title: "Titulo 5", text: "Texto 5", lastModificationDate: "18:04 08/06/2021"},
+        ]
+        const tempNoteId: Array<number> = [0, 1, 2, 3, 4]
+
+        await writeNote(tempNoteList)
+        await writeNoteId(tempNoteId)
+
+        setNote(tempNoteList)
+
+        console.log("note, noteId - WRITE")
+    }, [])
+
+    const debugClearNote = useCallback(async () => {
+        await writeNote([])
+        await writeNoteId([])
+
+        setNote([])
+
+        console.log("note, noteId - CLEAR")
+    }, [])
+
+
+    useEffect(() => {
+        createAllFolder()
+        debugGetDebugHome()
+        // getNote()
+    }, [])
+
+
     return (
         <SafeScreen>
             <HomeHeader
@@ -19,24 +97,28 @@ export function Home() {
                 exportNote={() => {}}
                 encryptFile={() => {}}
                 openSettings={() => {}}
-                switchDebugHome={() => {}}
+                switchDebugHome={debugSwitchDebugHome}
             />
 
             <View style={{flex: 1}} />
 
-            <EmptyListView>
-                <EmptyListImage source={appIconOutline} />
+            {(note.length === 0) && (
+                <EmptyListView>
+                    <EmptyListImage source={appIconOutline} />
 
-                <EmptyListText>
-                    Nenhuma nota
-                </EmptyListText>
-            </EmptyListView>
+                    <EmptyListText>
+                        Nenhuma nota
+                    </EmptyListText>
+                </EmptyListView>
+            )}
 
-            <DebugHome
-                debugReadDocument={() => {}}
-                debugWriteDocument={() => {}}
-                debugClearDocument={() => {}}
-            />
+            {(debugHome === "show") && (
+                <DebugHome
+                    debugReadNote={debugReadNote}
+                    debugWriteNote={debugWriteNote}
+                    debugClearNote={debugClearNote}
+                />
+            )}
         </SafeScreen>
     )
 }
