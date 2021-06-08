cedea4b6492c2e7dba025733604774083f4d5863
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-08 09:36:02 -0300

    Add KeepAwake while in development

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
-import React from "react"
+import React, { useEffect } from "react"
+import KeepAwake from "react-native-keep-awake"
 
 import { Router } from "./router"
 
 
 export function App() {
+
+
+    useEffect(() => {
+        if (__DEV__) {
+            KeepAwake.activate()
+
+            return () => KeepAwake.deactivate()
+        }
+    }, [])
+
+
     return (
         <Router />
     )
