94c749e211ad682bdd7fa37e10e9dcffbdca321f
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-09 01:44:25 -0300

    Add SubHeaderPath component

---
+++ src/component/SubHeaderPath/index.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import { TextProps } from "react-native"
+import styled from "styled-components/native"
+
+import { styledProps } from "../../service/theme"
+
+
+export const SubHeader = styled.View`
+    align-items: flex-start;
+    justify-content: center;
+    width: 100%;
+    height: 28px;
+    padding: 4px 16px;
+    background-color: ${(props: styledProps) => props.theme.color.subHeader_background};
+    elevation: 4;
+`
+
+
+export const SubHeaderTextBase = styled.Text`
+    width: 100%;
+    font-size: 13px;
+    opacity: ${(props: styledProps) => props.theme.opacity.mediumEmphasis};
+    color: ${(props: styledProps) => props.theme.color.subHeader_color};
+`
+
+
+export interface SubHeaderTextProps extends TextProps {
+    children?: string
+}
+
+
+export function SubHeaderText(props: SubHeaderTextProps) {
+    return (
+        <SubHeaderTextBase
+            numberOfLines={1}
+            ellipsizeMode={"head"}
+            {...props}
+        />
+    )
+}
