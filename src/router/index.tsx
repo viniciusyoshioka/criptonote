7840e14d56dfa2a5003fccbfc341eeedbf4d6945
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-09 16:21:06 -0300

    Read screen added to router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,33 +1,26 @@
 import React from "react"
 import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "react-native-screens/native-stack"
-import { enableScreens } from "react-native-screens"
+import { createStackNavigator } from "@react-navigation/stack"
 
 import { Home } from "../screen/Home"
 import { Settings } from "../screen/Settings"
 import { Add } from "../screen/Add"
 import { Code } from "../screen/Code"
+import { Read } from "../screen/Read"
 
 
-enableScreens()
-const Stack = createNativeStackNavigator()
+const Stack = createStackNavigator()
 
 
 export function Router() {
     return (
         <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName={"Home"}
-                screenOptions={{
-                    headerShown: false,
-                    replaceAnimation: "push",
-                    stackAnimation: "fade",
-                }}
-            >
+            <Stack.Navigator initialRouteName={"Home"} screenOptions={{headerShown: false}}>
                 <Stack.Screen name={"Home"} component={Home} />
                 <Stack.Screen name={"Add"} component={Add} />
                 <Stack.Screen name={"Code"} component={Code} />
                 <Stack.Screen name={"Settings"} component={Settings} />
+                <Stack.Screen name={"Read"} component={Read} />
             </Stack.Navigator>
         </NavigationContainer>
     )
