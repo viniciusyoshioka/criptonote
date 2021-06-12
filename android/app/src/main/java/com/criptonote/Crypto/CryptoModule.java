fa7c21d46ee32ec0d16ee17a3b4cae2a215dbf20
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-12 17:25:26 -0300

    Update Crypto to fix crash when decrypting

--- android/app/src/main/java/com/criptonote/Crypto/CryptoModule.java
+++ android/app/src/main/java/com/criptonote/Crypto/CryptoModule.java
@@ -1,6 +1,8 @@
 package com.criptonote.Crypto;
 
+import android.annotation.SuppressLint;
 import android.util.Base64;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
@@ -10,7 +12,12 @@ import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 
 public class CryptoModule extends ReactContextBaseJavaModule {
@@ -34,50 +41,45 @@ public class CryptoModule extends ReactContextBaseJavaModule {
     }
 
 
+    private SecretKey generateKey(byte[] password) throws Exception {
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        byte[] hashPassword = messageDigest.digest(password);
+        return new SecretKeySpec(hashPassword, "AES");
+    }
+
+
     @ReactMethod
-    public void encrypt(String text, String password, Promise promise) {
-        byte[] byteText = text.getBytes();
-        byte[] bytePassword = password.getBytes();
+    public void encrypt(String text, String password, Promise promise) throws Exception {
+        byte[] byteText = text.getBytes(StandardCharsets.UTF_8);
+        byte[] bytePassword = password.getBytes(StandardCharsets.UTF_8);
 
         // Generate key
-        SecretKey key = null;
-        try {
-            key = Crypto.generateKey(bytePassword);
-        } catch (Exception e) {
-            promise.reject("Error", "Couldn't generate key. Message: " + e.getMessage());
-        }
+        SecretKey key = generateKey(bytePassword);
 
         // Encrypt
-        String encryptedText = null;
-        try {
-            encryptedText = Crypto.encrypt(byteText, key);
-        } catch (Exception e) {
-            promise.reject("Error", "Couldn't encrypt text. Message: " + e.getMessage());
-        }
+        @SuppressLint("GetInstance")
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] byteEncryptedText = cipher.doFinal(byteText);
+        String encryptedText = Base64.encodeToString(byteEncryptedText, Base64.DEFAULT);
 
         promise.resolve(encryptedText);
     }
 
     @ReactMethod
-    public void decrypt(String text, String password, Promise promise) {
-        byte[] byteEncryptedText = Base64.decode(text, Base64.NO_WRAP);
-        byte[] bytePassword = password.getBytes();
+    public void decrypt(String text, String password, Promise promise) throws Exception {
+        byte[] byteEncryptedText = Base64.decode(text, Base64.DEFAULT);
+        byte[] bytePassword = password.getBytes(StandardCharsets.UTF_8);
 
         // Generate key
-        SecretKey key = null;
-        try {
-            key = Crypto.generateKey(bytePassword);
-        } catch (Exception e) {
-            promise.reject("Error", "Couldn't generate key. Message: " + e.getMessage());
-        }
+        SecretKey key = generateKey(bytePassword);
 
         // Decrypt
-        String decryptedText = null;
-        try {
-            decryptedText = Crypto.decrypt(byteEncryptedText, key);
-        } catch (Exception e) {
-            promise.reject("Error", "Couldn't decrypt text. Message: " + e.getMessage());
-        }
+        @SuppressLint("GetInstance")
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] byteDecryptedText = cipher.doFinal(byteEncryptedText);
+        String decryptedText = new String(byteDecryptedText, StandardCharsets.UTF_8);
 
         promise.resolve(decryptedText);
     }
@@ -86,34 +88,42 @@ public class CryptoModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void test(String text, String password) throws Exception {
         String encryptedText = returnEncrypt(text, password);
-        System.out.println("ENCRYPT: \"" + encryptedText + "\"");
+        Log.w("ALERTA", "ENCRYPT: \"" + encryptedText + "\"");
 
         String decryptedText = returnDecrypt(encryptedText, password);
-        System.out.println("DECRYPT: \"" + decryptedText + "\"");
+        Log.w("ALERTA", "DECRYPT: \"" + decryptedText + "\"");
 
-        System.out.println("RESULT: \"" + text.equals(decryptedText) + "\"");
+        Log.w("ALERTA", "RESULT: \"" + text.equals(decryptedText) + "\"");
     }
 
 
     private String returnEncrypt(String text, String password) throws Exception {
-        byte[] byteText = text.getBytes();
-        byte[] bytePassword = password.getBytes();
+        byte[] byteText = text.getBytes(StandardCharsets.UTF_8);
+        byte[] bytePassword = password.getBytes(StandardCharsets.UTF_8);
 
         // Generate key
-        SecretKey key = Crypto.generateKey(bytePassword);
+        SecretKey key = generateKey(bytePassword);
 
         // Encrypt
-        return Crypto.encrypt(byteText, key);
+        @SuppressLint("GetInstance")
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] byteEncryptedText = cipher.doFinal(byteText);
+        return Base64.encodeToString(byteEncryptedText, Base64.DEFAULT);
     }
 
     private String returnDecrypt(String text, String password) throws Exception {
-        byte[] byteText = text.getBytes();
-        byte[] bytePassword = password.getBytes();
+        byte[] byteEncryptedText = Base64.decode(text, Base64.DEFAULT);
+        byte[] bytePassword = password.getBytes(StandardCharsets.UTF_8);
 
         // Generate key
-        SecretKey key = Crypto.generateKey(bytePassword);
+        SecretKey key = generateKey(bytePassword);
 
         // Decrypt
-        return Crypto.decrypt(byteText, key);
+        @SuppressLint("GetInstance")
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] byteDecryptedText = cipher.doFinal(byteEncryptedText);
+        return new String(byteDecryptedText, StandardCharsets.UTF_8);
     }
 }
