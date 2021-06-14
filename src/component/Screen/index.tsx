302b7b4e7525caa16c62ff7d18e2a7d513ad551e
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-14 17:59:07 -0300

    Create screen CenterScreen

--- src/component/Screen/index.tsx
+++ src/component/Screen/index.tsx
@@ -13,3 +13,10 @@ export const SpaceScreen = styled.View`
     flex: 1;
     margin: 8px;
 `
+
+
+export const CenterScreen = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`
