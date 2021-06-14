613d090599ffabf1276620c8d7b21bd04c6d92c5
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-14 11:01:28 -0300

    Add CheckButton component

---
+++ src/component/CheckButton/index.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+import { TouchableOpacityProps } from "react-native"
+import CheckBox from "@react-native-community/checkbox"
+
+import { Button, Text } from "./style"
+import { useTheme } from "../../service/theme"
+
+
+export interface CheckButtonProps extends TouchableOpacityProps {
+    text?: string,
+    value?: boolean,
+    onValueChange: (newValue: boolean) => void,
+}
+
+
+export function CheckButton(props: CheckButtonProps) {
+
+
+    const { color, opacity } = useTheme()
+
+
+    return (
+        <Button {...props}>
+            <CheckBox
+                value={props.value}
+                onValueChange={props.onValueChange}
+                tintColors={{
+                    true: color.checkButton_checked_color,
+                    false: color.checkButton_unchecked_color,
+                }}
+                style={{opacity: opacity.highEmphasis}}
+            />
+
+            {props.text && (
+                <Text>
+                    {props.text}
+                </Text>
+            )}
+        </Button>
+    )
+}
