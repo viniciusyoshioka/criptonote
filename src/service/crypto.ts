2e17f42e252372db49c7b918da4d62f744a12c48
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-14 14:43:56 -0300

    Add functions in Crypto to encrypt/decrypt files

--- src/service/crypto.ts
+++ src/service/crypto.ts
@@ -5,6 +5,10 @@ export interface CryptoProps {
     testString: (text: string, password: string) => void,
     encryptString: (text: string, password: string) => Promise<string>,
     decryptString: (text: string, password: string) => Promise<string>,
+
+    testFile: (path: string, password: string) => void,
+    encryptFile: (path: string, password: string) => Promise<string>,
+    decryptFile: (path: string, password: string) => Promise<string>,
 }
 
 
