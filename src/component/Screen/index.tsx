cb20142f1067d440210a95511f3e4c0c83d1d079
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-14 09:24:38 -0300

    Add new screen 'SpaceScreen' for margin space between screen and children

--- src/component/Screen/index.tsx
+++ src/component/Screen/index.tsx
@@ -7,3 +7,9 @@ export const SafeScreen = styled.SafeAreaView`
     flex: 1;
     background-color: ${(props: styledProps) => props.theme.color.screen_background};
 `
+
+
+export const SpaceScreen = styled.View`
+    flex: 1;
+    margin: 8px;
+`
