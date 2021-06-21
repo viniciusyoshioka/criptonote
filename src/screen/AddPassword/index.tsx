365f33f8f71fafc3bacd8815673b2a9e72fedbea
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-21 13:41:34 -0300

    Created screen for add password in App

---
+++ src/screen/AddPassword/index.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+
+export function AddPassword() {
+    return null
+}
---
+++ src/screen/ChoosePasswordType/index.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+
+export function ChoosePasswordType() {
+    return null
+}
