a9772bb9181bbba907d62745c79c0126dc39a971
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-22 15:53:00 -0300

    Lock screen works to lock app when opening and to protect against password change in settings

--- src/screen/Lock/index.tsx
+++ src/screen/Lock/index.tsx
@@ -1,23 +1,37 @@
 import React, { createRef, useEffect, useState } from "react"
-import { Alert, TextInput, useWindowDimensions } from "react-native"
-import { useNavigation } from "@react-navigation/core"
+import { Alert, TextInput } from "react-native"
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/core"
 
-import { AppName, PasswordInput } from "./style"
+import { AppName } from "./style"
 import { SafeScreen } from "../../component/Screen"
-import { useKeyboard } from "../../service/hook"
-import { readLock } from "../../service/storage"
+import { useBackHandler, useKeyboard } from "../../service/hook"
+import { readLock, readLockType } from "../../service/storage"
 import { sha256 } from "../../service/message-digest"
+import { lockType } from "../../service/object-type"
+import { AddText } from "../../component/AddText"
+import { AddPin } from "../../component/AddPin"
+import { AddBio } from "../../component/AddBio"
+import { ScreenParams } from "../../service/screen-params"
 
 
 export function Lock() {
 
 
     const navigation = useNavigation()
+    const { params } = useRoute<RouteProp<ScreenParams, "Lock">>()
 
     const inputLockRef = createRef<TextInput>()
 
-    const [lock, setLock] = useState<string | null>(null)
+    const [appLockType, setAppLockType] = useState<lockType | null>(null)
 
+
+    useBackHandler(() => {
+        if (params?.action === "change-lock") {
+            navigation.navigate("Settings")
+            return true
+        }
+        return false
+    })
 
     useKeyboard("keyboardDidHide", () => {
         if (inputLockRef.current?.isFocused()) {
@@ -26,37 +40,44 @@ export function Lock() {
     })
 
 
-    async function unlock() {
-        if (lock !== null) {
-            const appLock = await readLock()
-            const hashLock = sha256(lock)
+    async function unlock(lock: string) {
+        const appLock = await readLock()
+        const hashLock = sha256(lock)
 
-            if (hashLock === appLock) {
+        if (hashLock === appLock) {
+            if (params?.action === "change-lock") {
                 navigation.reset({routes: [
-                    {name: "Home"}
+                    {name: "Home"},
+                    {name: "Settings"},
+                    {name: "ChoosePasswordType"},
                 ]})
                 return
             }
 
-            Alert.alert(
-                "Alerta",
-                "Senha inválida"
-            )
+            navigation.reset({routes: [
+                {name: "Home"}
+            ]})
+            return
         }
+
+        Alert.alert(
+            "Alerta",
+            "Senha inválida"
+        )
     }
 
 
     useEffect(() => {
         async function isProtected() {
-            const appLock = await readLock()
-            if (appLock === "") {
+            const readAppLockType = await readLockType()
+            if (readAppLockType === "none") {
                 navigation.reset({routes: [
                     {name: "Home"}
                 ]})
                 return
             }
 
-            setLock("")
+            setAppLockType(readAppLockType)
             inputLockRef.current?.focus()
         }
 
@@ -64,31 +85,36 @@ export function Lock() {
     }, [])
 
 
-    if (lock === null) {
+    if (appLockType === null) {
         return null
     }
 
 
     return (
-        <SafeScreen style={{alignItems: "center", justifyContent: "flex-start"}}>
+        <SafeScreen style={{justifyContent: "center"}}>
             <AppName>
                 CriptoNote
             </AppName>
 
-            <PasswordInput
-                style={{width: useWindowDimensions().width / 2}}
-                autoCapitalize={"none"}
-                autoCompleteType={"off"}
-                autoCorrect={false}
-                keyboardType={"numeric"}
-                onChangeText={(newText: string) => setLock(newText)}
-                onSubmitEditing={unlock}
-                placeholder={"Senha"}
-                ref={inputLockRef}
-                secureTextEntry={true}
-                textAlign={"center"}
-                value={lock}
-            />
+            {(appLockType === "pin") && (
+                <AddPin
+                    onDone={unlock}
+                    style={{marginTop: -100}}
+                />
+            )}
+
+            {(appLockType === "text") && (
+                <AddText
+                    onDone={unlock}
+                    style={{marginTop: -100}}
+                />
+            )}
+
+            {(appLockType === "bio") && (
+                <AddBio
+                    style={{marginTop: -100}}
+                />
+            )}
         </SafeScreen>
     )
 }
--- src/screen/Lock/style.tsx
+++ src/screen/Lock/style.tsx
@@ -1,23 +1,12 @@
 import styled from "styled-components/native"
 
-import { Input } from "../../component/Input"
 import { styledProps } from "../../service/theme"
 
 
 export const AppName = styled.Text`
+    text-align: center;
     margin-top: 100px;
-    margin-bottom: 50px;
     font-size: 22px;
     color: ${(props: styledProps) => props.theme.color.screen_color};
     opacity: ${(props: styledProps) => props.theme.opacity.highEmphasis};
-`
-
-
-export const PasswordInput = styled(Input)`
-    padding-left: 20px;
-    padding-right: 20px;
-    font-size: 20px;
-    border-bottom-width: 2px;
-    background-color: transparent;
-    border-color: ${(props: styledProps) => props.theme.color.screen_color};
 `
