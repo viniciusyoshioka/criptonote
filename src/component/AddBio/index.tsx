c3f9d4108f66cad6d32a1efcb1f709ec39786806
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-22 20:27:53 -0300

    Add 'TODO' in code

--- src/component/AddBio/index.tsx
+++ src/component/AddBio/index.tsx
@@ -7,6 +7,7 @@ import { CenterScreen } from "../Screen"
 export interface AddBioProps extends ViewProps {}
 
 
+// TODO
 export function AddBio(props: AddBioProps) {
     return (
         <CenterScreen {...props} />
