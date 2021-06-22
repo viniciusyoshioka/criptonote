94b37f4a72cc5f0cb36174f575d3aeeac93e46e0
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-06-21 21:48:15 -0300

    Change radio check of RadioButton to icon

--- src/component/RadioButton/index.tsx
+++ src/component/RadioButton/index.tsx
@@ -1,7 +1,9 @@
 import React from "react"
 import { TouchableOpacityProps } from "react-native"
+import Icon from "react-native-vector-icons/MaterialIcons"
 
-import { Button, Check, Radio, Text } from "./style"
+import { useTheme } from "../../service/theme"
+import { Button, Text } from "./style"
 
 
 export interface RadioButtonProps extends TouchableOpacityProps {
@@ -11,11 +13,18 @@ export interface RadioButtonProps extends TouchableOpacityProps {
 
 
 export function RadioButton(props: RadioButtonProps) {
+
+
+    const { color } = useTheme()
+
+
     return (
-        <Button {...props}>
-            <Radio checked={props.value ? true : false}>
-                {props.value && <Check checked={props.value ? true : false} />}
-            </Radio>
+        <Button {...props} activeOpacity={0.7}>
+            <Icon
+                name={props.value ? "radio-button-checked" : "radio-button-unchecked"}
+                size={24}
+                color={props.value ? color.radioButton_checked_color : color.radioButton_unchecked_color}
+            />
 
             {props.text && (
                 <Text>
