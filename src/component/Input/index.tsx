7c9a47b92bd742fd273de7b8364eadde90d616d5
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-07-04 15:14:37 -0300

    Base Input component has border color when focused

--- src/component/Input/index.tsx
+++ src/component/Input/index.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components/native"
 import { styledProps, useTheme } from "../../service/theme"
 
 
-export const InputBase = styled.TextInput`
+const InputBase = styled.TextInput`
     height: 48px;
     padding-vertical: 0px;
     padding-left: 16px;
@@ -14,14 +14,19 @@ export const InputBase = styled.TextInput`
     border-radius: 2px;
     background-color: ${(props: styledProps) => props.theme.color.input_background};
     color: ${(props: styledProps) => props.theme.color.input_color};
-    border-color: ${(props: styledProps & { isFocused: boolean }) => {
+    border-color: ${(props: styledProps & { isFocused?: boolean }) => {
         return props.isFocused ? props.theme.color.input_focus_border : props.theme.color.input_unfocus_border
     }};
     border-bottom-width: 2px;
 `
 
 
-export const Input = forwardRef((props: TextInputProps, ref?: Ref<TextInput>) => {
+export interface InputProps extends TextInputProps {
+    isFocused?: boolean,
+}
+
+
+export const Input = forwardRef((props: InputProps, ref?: Ref<TextInput>) => {
 
 
     const { color } = useTheme()
@@ -32,12 +37,26 @@ export const Input = forwardRef((props: TextInputProps, ref?: Ref<TextInput>) =>
     return (
         <InputBase
             blurOnSubmit={false}
-            placeholderTextColor={props.placeholderTextColor || color.input_placeholder}
+            placeholderTextColor={color.input_placeholder}
             ref={ref}
-            onFocus={() => setIsFocused(true)}
-            onBlur={() => setIsFocused(false)}
             selectionColor={color.input_selection}
-            isFocused={isFocused}
+            onBlur={(e) => {
+                if (props.isFocused === undefined) {
+                    setIsFocused(false)
+                }
+                if (props.onBlur) {
+                    props.onBlur(e)
+                }
+            }}
+            onFocus={(e) => {
+                if (props.isFocused === undefined) {
+                    setIsFocused(true)
+                }
+                if (props.onFocus) {
+                    props.onFocus(e)
+                }
+            }}
+            isFocused={props.isFocused !== undefined ? props.isFocused : isFocused}
             {...props}
         />
     )
