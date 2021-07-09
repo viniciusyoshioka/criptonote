4caca582db8a82d0473dc3a6602ac617319cf043
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-07-09 20:48:15 -0300

    Add index.ts for  components

---
+++ src/component/index.ts
@@ -0,0 +1,58 @@
+import { AddBio, AddBioProps } from "./AddBio"
+import { AddPin, AddPinProps } from "./AddPin"
+import { AddText, AddTextProps } from "./AddText"
+import { CheckButton, CheckButtonProps } from "./CheckButton"
+import { DebugButton, DebugButtonProps } from "./DebugButton"
+import { EmptyList, EmptyListProps } from "./EmptyList"
+import { Header, HeaderButton, HeaderButtonProps, HeaderTitle, HeaderTitleProps } from "./Header"
+import { Input, InputProps } from "./Input"
+import { InputPassword } from "./InputPassword"
+import { InputText } from "./InputText"
+import { InputTitle } from "./InputTitle"
+import { ListItem, ListItemProps } from "./ListItem"
+import { MenuItem, MenuItemProps } from "./MenuItem"
+import { Modal, ModalProps } from "./Modal"
+import { ModalButton, ModalButtonProps, ModalTitle, ModalViewButton, ModalViewContent } from "./ModalComponent"
+import { MyModal, MyModalProps } from "./MyModal"
+import { NoteItem, NoteItemProps } from "./NoteItem"
+import { RadioButton, RadioButtonProps } from "./RadioButton"
+import { CenterScreen, SafeScreen, SpaceScreen } from "./Screen"
+import { SubHeader, SubHeaderText, SubHeaderTextProps } from "./SubHeader"
+
+
+export { AddBio }
+export type { AddBioProps }
+export { AddPin }
+export type { AddPinProps }
+export { AddText }
+export type { AddTextProps }
+export { CheckButton }
+export type { CheckButtonProps }
+export { DebugButton }
+export type { DebugButtonProps }
+export { EmptyList }
+export type { EmptyListProps }
+export { Header, HeaderButton, HeaderTitle }
+export type { HeaderButtonProps, HeaderTitleProps }
+export { Input }
+export type { InputProps }
+export { InputPassword }
+export { InputText }
+export { InputTitle }
+export { ListItem }
+export type { ListItemProps }
+export { MenuItem }
+export type { MenuItemProps }
+export { Modal }
+export type { ModalProps }
+export { ModalButton, ModalTitle, ModalViewButton, ModalViewContent }
+export type { ModalButtonProps }
+export { MyModal }
+export type { MyModalProps }
+export { NoteItem }
+export type { NoteItemProps }
+export { RadioButton }
+export type { RadioButtonProps }
+export { CenterScreen, SafeScreen, SpaceScreen }
+export { SubHeader, SubHeaderText }
+export type { SubHeaderTextProps }
