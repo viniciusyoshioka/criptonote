0e502ef6c75dcdff77541eb5b06b8cae523e5c79
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-07-15 23:15:04 -0300

    Removed expo-local-authenticatin library

--- android/app/src/main/java/com/criptonote/MainApplication.java
+++ android/app/src/main/java/com/criptonote/MainApplication.java
@@ -1,7 +1,5 @@
 package com.criptonote;
 
-import com.criptonote.generated.BasePackageList;
-
 import android.app.Application;
 import android.content.Context;
 
@@ -16,15 +14,9 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Arrays;
- 
-import org.unimodules.adapters.react.ModuleRegistryAdapter;
-import org.unimodules.adapters.react.ReactModuleRegistryProvider;
-import org.unimodules.core.interfaces.SingletonModule;
 
 
 public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
@@ -38,10 +30,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
-            new ModuleRegistryAdapter(mModuleRegistryProvider)
-          );
-          packages.addAll(unimodules);
           packages.add(new CryptoPackage());
           return packages;
         }
