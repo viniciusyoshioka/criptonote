f3fbe94e3da8af413b21c43331d5b407e9b79884
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-07-17 13:37:18 -0300

    Update android project settings

--- android/app/src/main/java/com/criptonote/MainApplication.java
+++ android/app/src/main/java/com/criptonote/MainApplication.java
@@ -13,7 +13,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import java.util.Arrays;
 
 
 public class MainApplication extends Application implements ReactApplication {
@@ -27,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
