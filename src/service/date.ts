244ab14f56245ea168e5e2dab21df794e2e2ff19
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-11-15 21:50:06 -0300

    Fix date and time returned by getDate, getTime and getDateTime

--- src/service/date.ts
+++ src/service/date.ts
@@ -1,25 +1,23 @@
 
-export function getDate(currentDate=new Date(), separator="/"): string {
-    let localeDate = currentDate.toLocaleDateString()
-    if (separator !== "/") {
-        localeDate = localeDate.replace("/", separator)
-        localeDate = localeDate.replace("/", separator)
-    }
-    return localeDate
+export function getDate(separator="/", currentDate=new Date()): string {
+    const day = currentDate.getDate()
+    const month = currentDate.getMonth()
+    const year = currentDate.getFullYear()
+
+    return `${day}${separator}${month}${separator}${year}`
 }
 
 
-export function getTime(currentDate=new Date(), separator=":", hasSecond=false): string {
-    let localeTime = currentDate.toLocaleTimeString()
-    if (separator !== ":") {
-        localeTime = localeTime.replace(":", separator)
-        localeTime = localeTime.replace(":", separator)
+export function getTime(separator=":", hasSecond=false, currentDate=new Date()): string {
+    const hour = currentDate.getHours()
+    const minute = currentDate.getMinutes()
+
+    let localeTime = `${hour}${separator}${minute}`
+    if (hasSecond) {
+        const second = currentDate.getSeconds()
+        localeTime += `${separator}${second}`
     }
-    if (!hasSecond) {
-        const splitLocaleTime = localeTime.split(separator)
-        splitLocaleTime.length = 2
-        localeTime = splitLocaleTime.join(separator)
-    }
+
     return localeTime
 }
 
@@ -27,8 +25,8 @@ export function getTime(currentDate=new Date(), separator=":", hasSecond=false):
 export function getDateTime(dateSeparator="/", timeSeparator=":", hasSecond=false): string {
     const currentDate = new Date()
 
-    const date = getDate(currentDate, dateSeparator)
-    const time = getTime(currentDate, timeSeparator, hasSecond)
+    const date = getDate(dateSeparator, currentDate)
+    const time = getTime(timeSeparator, hasSecond, currentDate)
 
     return `${date} ${time}`
 }
