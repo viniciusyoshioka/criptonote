586ed47115570fd8876db2d0f80b0cd27cb7754a
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-11-27 10:25:10 -0300

    Add getReadPermission and getWritePermission of external storage

--- src/service/permission.ts
+++ src/service/permission.ts
@@ -26,3 +26,23 @@ export async function getLogPermission(): Promise<LogPermissionResult> {
         READ_EXTERNAL_STORAGE: status["android.permission.READ_EXTERNAL_STORAGE"] === "granted"
     }
 }
+
+
+export async function getReadPermission(): Promise<boolean> {
+    const hasPermission = await PermissionsAndroid.check("android.permission.READ_EXTERNAL_STORAGE")
+    if (!hasPermission) {
+        const requestedPermission = await PermissionsAndroid.request("android.permission.READ_EXTERNAL_STORAGE")
+        return requestedPermission === "granted"
+    }
+    return true
+}
+
+
+export async function getWritePermission(): Promise<boolean> {
+    const hasPermission = await PermissionsAndroid.check("android.permission.WRITE_EXTERNAL_STORAGE")
+    if (!hasPermission) {
+        const requestedPermission = await PermissionsAndroid.request("android.permission.WRITE_EXTERNAL_STORAGE")
+        return requestedPermission === "granted"
+    }
+    return true
+}
