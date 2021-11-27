5870eafc17a88d0547d4a5b82728c84d52b6c383
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-11-27 20:23:55 -0300

    Remove getLogPermission and LogPermissionResult from permissions

--- src/service/permission.ts
+++ src/service/permission.ts
@@ -1,33 +1,6 @@
 import { PermissionsAndroid } from "react-native"
 
 
-export type LogPermissionResult = {
-    WRITE_EXTERNAL_STORAGE: boolean,
-    READ_EXTERNAL_STORAGE: boolean,
-}
-
-export async function getLogPermission(): Promise<LogPermissionResult> {
-    const hasWritePermission = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE)
-    const hasReadPermission = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE)
-    if (hasWritePermission && hasReadPermission) {
-        return {
-            WRITE_EXTERNAL_STORAGE: true,
-            READ_EXTERNAL_STORAGE: true
-        }
-    }
-
-    const status = await PermissionsAndroid.requestMultiple([
-        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE, 
-        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE
-    ])
-
-    return {
-        WRITE_EXTERNAL_STORAGE: status["android.permission.WRITE_EXTERNAL_STORAGE"] === "granted",
-        READ_EXTERNAL_STORAGE: status["android.permission.READ_EXTERNAL_STORAGE"] === "granted"
-    }
-}
-
-
 export async function getReadPermission(): Promise<boolean> {
     const hasPermission = await PermissionsAndroid.check("android.permission.READ_EXTERNAL_STORAGE")
     if (!hasPermission) {
