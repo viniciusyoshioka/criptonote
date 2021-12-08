3c4ec6d47a63744377b4bbd1e1f6cc844539954b
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-12-08 08:58:32 -0300

    Update index of component

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,67 +1,23 @@
-import { AddBio, AddBioProps } from "./AddBio"
-import { AddPin, AddPinProps } from "./AddPin"
-import { AddText, AddTextProps } from "./AddText"
-import { Button, ButtonProps } from "./Button"
-import { ButtonOutline, ButtonOutlineProps } from "./ButtonOutline"
-import { ButtonText, ButtonTextProps } from "./ButtonText"
-import { CheckButton, CheckButtonProps } from "./CheckButton"
-import { DebugButton, DebugButtonProps } from "./DebugButton"
-import { EmptyList, EmptyListProps } from "./EmptyList"
-import { Header, HeaderButton, HeaderButtonProps, HeaderTitle, HeaderTitleProps } from "./Header"
-import { Input, InputProps } from "./Input"
-import { InputPassword } from "./InputPassword"
-import { InputText } from "./InputText"
-import { InputTitle } from "./InputTitle"
-import { ListItem, ListItemProps } from "./ListItem"
-import { MenuItem, MenuItemProps } from "./MenuItem"
-import { Modal, ModalProps } from "./Modal"
-import { ModalButton, ModalButtonProps, ModalTitle, ModalViewButton, ModalViewContent } from "./ModalComponent"
-import { MyModal, MyModalProps } from "./MyModal"
-import { NoteItem, NoteItemProps } from "./NoteItem"
-import { RadioButton, RadioButtonProps } from "./RadioButton"
-import { CenterScreen, SafeScreen, SpaceScreen } from "./Screen"
-import { SubHeader, SubHeaderText, SubHeaderTextProps } from "./SubHeader"
-
-
-export { AddBio }
-export type { AddBioProps }
-export { AddPin }
-export type { AddPinProps }
-export { AddText }
-export type { AddTextProps }
-export { Button }
-export type { ButtonProps }
-export { ButtonOutline }
-export type { ButtonOutlineProps }
-export { ButtonText }
-export type { ButtonTextProps }
-export { CheckButton }
-export type { CheckButtonProps }
-export { DebugButton }
-export type { DebugButtonProps }
-export { EmptyList }
-export type { EmptyListProps }
-export { Header, HeaderButton, HeaderTitle }
-export type { HeaderButtonProps, HeaderTitleProps }
-export { Input }
-export type { InputProps }
-export { InputPassword }
-export { InputText }
-export { InputTitle }
-export { ListItem }
-export type { ListItemProps }
-export { MenuItem }
-export type { MenuItemProps }
-export { Modal }
-export type { ModalProps }
-export { ModalButton, ModalTitle, ModalViewButton, ModalViewContent }
-export type { ModalButtonProps }
-export { MyModal }
-export type { MyModalProps }
-export { NoteItem }
-export type { NoteItemProps }
-export { RadioButton }
-export type { RadioButtonProps }
-export { CenterScreen, SafeScreen, SpaceScreen }
-export { SubHeader, SubHeaderText }
-export type { SubHeaderTextProps }
+export * from "./AddBio"
+export * from "./AddPin"
+export * from "./AddText"
+export * from "./Button"
+export * from "./ButtonOutline"
+export * from "./ButtonText"
+export * from "./CheckButton"
+export * from "./DebugButton"
+export * from "./EmptyList"
+export * from "./Header"
+export * from "./Input"
+export * from "./InputPassword"
+export * from "./InputText"
+export * from "./InputTitle"
+export * from "./ListItem"
+export * from "./MenuItem"
+export * from "./Modal"
+export * from "./ModalComponent"
+export * from "./MyModal"
+export * from "./NoteItem"
+export * from "./RadioButton"
+export * from "./Screen"
+export * from "./SubHeader"
