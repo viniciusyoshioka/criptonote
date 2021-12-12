f535fcdf4a3d76d7d1397a3fd8861edc2f3fa2c4
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-12-12 11:35:31 -0300

    Update app version

--- src/service/constant.ts
+++ src/service/constant.ts
@@ -4,7 +4,7 @@ import RNFS from "react-native-fs"
 
 // App
 export const appName = "CriptoNote"
-export const appVersion = "6.0.0"
+export const appVersion = "6.1.0"
 export const appType = __DEV__ ? "beta" : "release"
 
 // Icon
