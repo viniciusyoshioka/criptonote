aad322105e4941f9a5a29c93eaa555953e158909
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-01-07 21:49:17 -0300

    Removed unused eslint-disable comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React, { useEffect, useState } from "react"
 import { useColorScheme } from "react-native"
 import KeepAwake from "react-native-keep-awake"
