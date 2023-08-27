70da28a03cae23bddaf1882f55bdab282f3b1608
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-08-27 20:15:26 -0300

    Formated .babelrc, babel.config.js and index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import "react-native-gesture-handler"
 import { AppRegistry } from "react-native"
-import { App } from "./src"
+import "react-native-gesture-handler"
+
 import { name as appName } from "./app.json"
+import { App } from "./src"
+
 
 AppRegistry.registerComponent(appName, () => App)
