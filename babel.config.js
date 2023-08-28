f7ec1c2fe1bbcb37ea8fa3bf71066b3f28571623
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-08-27 21:22:50 -0300

    Installed babel-plugin-module-resolver and configured path alias

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
 module.exports = {
     presets: ["module:metro-react-native-babel-preset"],
     plugins: [
+        [
+            "module-resolver",
+            {
+                root: ["."],
+                extensions: [".js", ".ts", ".tsx", ".json"],
+                alias: {
+                    "@components": "./src/components",
+                    "@database": "./src/database",
+                    "@hooks": "./src/hooks",
+                    "@locales": "./src/locales",
+                    "@router": "./src/router",
+                    "@screen": "./src/screen",
+                    "@services": "./src/services",
+                    "@theme": "./src/theme",
+                },
+            }
+        ],
         ["@babel/plugin-proposal-decorators", { legacy: true } ],
         "@realm/babel-plugin",
     ],
