3f978ae1a633d82ccc0fb00db5238f73196a6f78
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-08-27 23:30:14 -0300

    Add new database provider in App()

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { MenuProvider } from "react-native-popup-menu"
 import { ThemeProvider } from "styled-components"
 
+import { RealmProvider } from "@database"
 import { useKeepAwakeOnDev } from "@hooks"
 import { Router } from "@router"
 import { DarkTheme, ThemeContextProvider } from "./service/theme"
@@ -15,13 +16,15 @@ export function App() {
 
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
-            <ThemeContextProvider value={DarkTheme}>
-                <ThemeProvider theme={DarkTheme}>
-                    <MenuProvider>
-                        <Router />
-                    </MenuProvider>
-                </ThemeProvider>
-            </ThemeContextProvider>
+            <RealmProvider>
+                <ThemeContextProvider value={DarkTheme}>
+                    <ThemeProvider theme={DarkTheme}>
+                        <MenuProvider>
+                            <Router />
+                        </MenuProvider>
+                    </ThemeProvider>
+                </ThemeContextProvider>
+            </RealmProvider>
         </GestureHandlerRootView>
     )
 }
