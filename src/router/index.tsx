719293bc69da3325650ce661a7eabaae26fa4251
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-08-27 23:53:04 -0300

    Refactor Router()

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,43 +1,31 @@
-import React from "react"
 import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { NativeStackNavigationOptions, createNativeStackNavigator } from "@react-navigation/native-stack"
 import { enableScreens } from "react-native-screens"
 
-import { Lock } from "../screen/Lock"
-import { Home } from "../screen/Home"
-import { Add } from "../screen/Add"
-import { Code } from "../screen/Code"
-import { Settings } from "../screen/Settings"
-import { Read } from "../screen/Read"
-import { FileExplorer } from "../screen/FileExplorer"
-import { FileEncryption } from "../screen/FileEncryption"
+import { Home } from "@screen/Home"
+import { ScreenParams } from "./types"
 
-import { AddPassword } from "../screen/AddPassword"
-import { ChoosePasswordType } from "../screen/ChoosePasswordType"
+
+export * from "./types"
 
 
 enableScreens()
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator<ScreenParams>()
 
 
 export function Router() {
+
+
+    const stackNavigatorScreenOptions: NativeStackNavigationOptions = {
+        animation: "fade",
+        headerShown: false,
+    }
+
+
     return (
         <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName={"Lock"}
-                screenOptions={{ headerShown: false }}
-            >
-                <Stack.Screen name={"Lock"} component={Lock} />
+            <Stack.Navigator initialRouteName={"Home"} screenOptions={stackNavigatorScreenOptions}>
                 <Stack.Screen name={"Home"} component={Home} />
-                <Stack.Screen name={"Add"} component={Add} />
-                <Stack.Screen name={"Code"} component={Code} />
-                <Stack.Screen name={"Settings"} component={Settings} />
-                <Stack.Screen name={"Read"} component={Read} />
-                <Stack.Screen name={"FileExplorer"} component={FileExplorer} />
-                <Stack.Screen name={"FileEncryption"} component={FileEncryption} />
-
-                <Stack.Screen name={"AddPassword"} component={AddPassword} />
-                <Stack.Screen name={"ChoosePasswordType"} component={ChoosePasswordType} />
             </Stack.Navigator>
         </NavigationContainer>
     )
