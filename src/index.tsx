d9643154f34e83b8242ce8520028e7708db77d04
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-08-27 23:28:37 -0300

    Removed old database usage from App()

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,10 @@
-import { useEffect, useState } from "react"
-import { useColorScheme } from "react-native"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { MenuProvider } from "react-native-popup-menu"
-import SQLite from "react-native-sqlite-storage"
 import { ThemeProvider } from "styled-components"
 
 import { useKeepAwakeOnDev } from "@hooks"
-import { LogDatabase, NoteDatabase, openAppDatabase, openLogDatabase, setGlobalAppDatabase, setGlobalLogDatabase, SettingsDatabase } from "./database"
-import { Router } from "./router"
-import { logCriticalError } from "./service/log"
-import { DarkTheme, LightTheme, ThemeContextProvider, themeType } from "./service/theme"
+import { Router } from "@router"
+import { DarkTheme, ThemeContextProvider } from "./service/theme"
 
 
 export function App() {
@@ -18,80 +13,10 @@ export function App() {
     useKeepAwakeOnDev()
 
 
-    const deviceTheme = useColorScheme()
-
-    const [appDb, setAppDb] = useState<SQLite.SQLiteDatabase | undefined>(undefined)
-    const [logDb, setLogDb] = useState<SQLite.SQLiteDatabase | undefined>(undefined)
-    const [theme, setTheme] = useState<themeType | undefined>()
-
-
-    async function getTheme() {
-        const appTheme = await SettingsDatabase.getSettingKey("theme")
-
-        LightTheme.appTheme = appTheme
-        LightTheme.switchTheme = switchTheme
-
-        DarkTheme.appTheme = appTheme
-        DarkTheme.switchTheme = switchTheme
-
-        if (appTheme === "auto") {
-            if (deviceTheme) {
-                setTheme(deviceTheme)
-                return
-            }
-            setTheme("light")
-            return
-        }
-        setTheme(appTheme)
-    }
-
-    async function switchTheme(newTheme: themeType) {
-        await SettingsDatabase.updateSettings("theme", newTheme)
-        await getTheme()
-    }
-
-
-    useEffect(() => {
-        if (appDb && logDb) {
-            getTheme()
-        }
-    }, [deviceTheme, appDb, logDb])
-
-    useEffect(() => {
-        SQLite.enablePromise(true)
-
-        openAppDatabase()
-            .then(async (database) => {
-                setGlobalAppDatabase(database)
-                await NoteDatabase.createNoteTable()
-                await SettingsDatabase.createSettingsTable()
-                setAppDb(database)
-            })
-            .catch((error) => {
-                logCriticalError(`Error opening app database: "${JSON.stringify(error)}"`)
-            })
-
-        openLogDatabase()
-            .then(async (database) => {
-                setGlobalLogDatabase(database)
-                await LogDatabase.createLogTable()
-                setLogDb(database)
-            })
-            .catch((error) => {
-                logCriticalError(`Error opening log database: "${JSON.stringify(error)}"`)
-            })
-    }, [])
-
-
-    if (!theme || !appDb || !logDb) {
-        return null
-    }
-
-
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
-            <ThemeContextProvider value={(theme === "light") ? LightTheme : DarkTheme}>
-                <ThemeProvider theme={(theme === "light") ? LightTheme : DarkTheme}>
+            <ThemeContextProvider value={DarkTheme}>
+                <ThemeProvider theme={DarkTheme}>
                     <MenuProvider>
                         <Router />
                     </MenuProvider>
