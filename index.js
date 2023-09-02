a38f0d4f1caccb6e1ffe6ab0d2317a0394932378
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-09-02 16:28:51 -0300

    Installed and configured react-native-get-random-values

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from "react-native"
 import "react-native-gesture-handler"
+import "react-native-get-random-values"
 
 import { name as appName } from "./app.json"
 import { App } from "./src"
