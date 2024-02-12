75781f81a3131e8387aeee02f87ab183738b45f1
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-02-12 12:13:14 -0300

    Update InputPassword's border

--- src/components/InputPassword/index.tsx
+++ src/components/InputPassword/index.tsx
@@ -1,18 +1,21 @@
-import { ForwardedRef, forwardRef, useState } from "react"
-import { NativeSyntheticEvent, StyleProp, StyleSheet, TextInput, TextInputFocusEventData, View, ViewStyle } from "react-native"
+import { Icon } from "@elementium/native"
+import { forwardRef, useState } from "react"
+import { NativeSyntheticEvent, Pressable, StyleProp, TextInput, TextInputFocusEventData, TextInputProps, View, ViewStyle } from "react-native"
+import { createStyleSheet, useStyles } from "react-native-unistyles"
 
 import { translate } from "@locales"
-import { Input, InputProps } from "../Input"
-import { ToggleVisibilityButton } from "./ToggleVisibilityButton"
 
 
-export interface InputPasswordProps extends InputProps {
+export interface InputPasswordProps extends TextInputProps {
     wrapperStyle?: StyleProp<ViewStyle>
 }
 
 
-export const InputPassword = forwardRef((props: InputPasswordProps, ref: ForwardedRef<TextInput>) => {
+export const InputPassword = forwardRef<TextInput, InputPasswordProps>((props, ref) => {
 
+
+    const { styles, theme } = useStyles(stylesheet)
+    const { colors } = theme
 
     const [hidePassword, setHidePassword] = useState(true)
     const [isFocused, setIsFocused] = useState(false)
@@ -38,38 +41,67 @@ export const InputPassword = forwardRef((props: InputPasswordProps, ref: Forward
 
     return (
         <View style={[styles.wrapper, props.wrapperStyle]}>
-            <Input
+            <TextInput
                 autoCapitalize={"none"}
                 autoComplete={"new-password"}
                 autoCorrect={false}
                 placeholder={translate("password")}
                 secureTextEntry={isPasswordHidden}
                 keyboardType={isPasswordHidden ? "default" : "visible-password"}
-                {...props}
+                blurOnSubmit={false}
+                placeholderTextColor={colors.onSurfaceVariant}
+                selectionColor={colors.primaryContainer}
+                cursorColor={colors.primary}
                 ref={ref}
-                isFocused={isFocused}
-                onFocus={onFocus}
+                {...props}
                 onBlur={onBlur}
-                style={[styles.input, props.style]}
+                onFocus={onFocus}
+                style={[styles.input(isFocused), props.style]}
             />
 
-            <ToggleVisibilityButton
-                hidePassword={isPasswordHidden}
-                isFocused={isFocused}
-                onPress={togglePasswordVisibility}
-            />
+            <Pressable onPress={togglePasswordVisibility} style={styles.toggleButton(isFocused)}>
+                <Icon
+                    name={isPasswordHidden ? "eye-off-outline" : "eye-outline"}
+                    group={"material-community"}
+                    color={colors.onSurfaceVariant}
+                />
+            </Pressable>
         </View>
     )
 })
 
 
-const styles = StyleSheet.create({
+const stylesheet = createStyleSheet(theme => ({
     wrapper: {
         flexDirection: "row",
         alignItems: "center",
     },
-    input: {
+    input: (isFocused: boolean) => ({
         flex: 1,
-        borderTopRightRadius: 0,
-    },
-})
+        height: 48,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+
+        ...theme.typography.body.large,
+
+        backgroundColor: theme.colors.surfaceContainerHighest,
+        color: theme.colors.onSurface,
+        borderTopLeftRadius: theme.shape.extraSmall,
+        borderBottomLeftRadius: theme.shape.extraSmall,
+        borderWidth: 2,
+        borderRightWidth: 0,
+        borderColor: isFocused ? theme.colors.primary : theme.colors.surfaceContainerHighest,
+    }),
+    toggleButton: (isFocused: boolean) => ({
+        alignItems: "center",
+        justifyContent: "center",
+        width: 48,
+        height: 48,
+        backgroundColor: theme.colors.surfaceContainerHighest,
+        borderTopRightRadius: theme.shape.extraSmall,
+        borderBottomRightRadius: theme.shape.extraSmall,
+        borderWidth: 2,
+        borderLeftWidth: 0,
+        borderColor: isFocused ? theme.colors.primary : theme.colors.surfaceContainerHighest,
+    }),
+}))
